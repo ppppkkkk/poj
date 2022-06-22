342b7f4ac11c39645ca9a666ebdc0beab987d8e5
ppppkkkk <45647667+ppppkkkk@users.noreply.github.com>
2022-06-22 21:36:05 +0800

    Create 1006.cpp

---
+++ 1006.cpp
@@ -0,0 +1,42 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#define M 40000
+ 
+using namespace std;
+
+bool ans[3][M];
+ 
+int main()
+{
+    int p, e, i, d;
+    int n=0;
+ 
+    while(cin>>p>>e>>i>>d, p >= 0)
+    {
+        memset(ans, 0, sizeof(ans));
+ 
+        p = p % 23;//得到在一个周期内高峰第几天
+        e = e % 28;
+        i = i % 33;
+ 
+        for(int j = p; j < M; j += 23)
+            ans[0][j]++;
+ 
+        for(int j = e; j < M; j += 28)
+            ans[1][j]++;
+ 
+        for(int j = i; j < M; j += 33)
+            ans[2][j]++;
+ 
+ 
+        for(int j = d + 1; j < M; j++)
+            if(ans[0][j] == 1 && ans[1][j] == 1 && ans[2][j] == 1 )
+        {
+            printf("Case %d: the next triple peak occurs in %d days.\n", ++n, j - d);
+            break;
+        }
+    }
+
+    return 0;
+}
