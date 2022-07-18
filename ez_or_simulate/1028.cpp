9ee31b0c0dbe4d24103c0fc2d9e85367d15d93b8
ppppkkkk <45647667+ppppkkkk@users.noreply.github.com>
2022-07-18 21:17:26 +0800

    Update and rename 1028.cpp to ez_or_simulate/1028.cpp

--- ez_or_simulate/1028.cpp
+++ ez_or_simulate/1028.cpp
@@ -5,6 +5,21 @@
 #include<algorithm>
 using namespace std;
 
+/*
+poj 1028栈模拟
+
+首先，我们的浏览器先访问http://www.acm.org/。
+然后，发出一些指令，每个指令发出后，打印指令结果。
+指令表：
+
+指令	含义	参数
+QUIT	退出	无
+BACK	返回上一个浏览的页面	无
+FORWARD	如果BACK是“撤回”，那么它相当于“重做”	无
+VISIT	访问一个新页面，之前执行“撤回”的url全部清空，此时无法执行“FORWARD”	url
+
+
+*/
 
 int main()
 {
