e38721d35241ecdd0d634aad35f797143bee125d
ppppkkkk <45647667+ppppkkkk@users.noreply.github.com>
2022-07-26 12:37:36 +0800

    Create 3012.cpp

---
+++ maths/3012.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+/*
+对于杨辉三角形的第n行，首先但看每一位，将其补成长度为k的数（左补0），然后将这一行连起来作为一个大数，求这个大数mod m。
+题解：数学公式推导题
+　　　1、补0实际上就是使得这个数左边的那个数乘以10的次方，那么最后答案就是Σ(C(n,m)*10^( (n-m)*k ) )%m
+　　　2、10^(n-m)k=(10^k)^(n-m),又有个C(n,m)，正好组成二项式(1+10^k)^n的展开式
+　　　3、于是两个快速幂取模就可以解决了。
+*/
+
+
+
+
+
+long long fastmod(long long a, long long k, long long m)
+{
+	long long re = 1, y = a % m;
+	for (;k;k >>= 1, y = y * y % m) 
+		if (k & 1ll) 
+			re = y * re % m;
+	return re;
+}
+
+int main()
+{
+	int t;
+	scanf("%d", &t);
+	while (t--)
+	{
+		long long n, k, m;
+		scanf("%lld%lld%lld", &n, &k, &m);
+		printf("%lld\n", fastmod(fastmod(10, k, m) + 1, n, m));
+	}
+
+	return 0;
+}
